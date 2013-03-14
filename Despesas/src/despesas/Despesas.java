a21ba5e38bc7123c49d909024a30ed846f83b180
i111057 <i111057@PCI113.dei.isep.ipp.pt>
2013-03-14 22:20:54 +0000

    adicionei um print

--- Despesas/src/despesas/Despesas.java
+++ Despesas/src/despesas/Despesas.java
@@ -14,6 +14,6 @@ public class Despesas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Eu tenho despesas!!!\n");
     }
 }
