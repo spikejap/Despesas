cc24ab7a3423d49cf9c13b83c7a9f6a6235a04fb
i111057 <i111057@PCI113.dei.isep.ipp.pt>
2013-03-14 22:15:16 +0000

    Inicio do projecto GIT

---
+++ Despesas/src/despesas/Despesas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package despesas;
+
+/**
+ *
+ * @author i111057
+ */
+public class Despesas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
